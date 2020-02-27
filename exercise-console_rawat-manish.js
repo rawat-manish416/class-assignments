b27f17446e03f630e1cb211ab635dc2045a25d59
Manish Singh Rawat <mrawat@isimtl.local>
2020-02-27 15:02:58 -0500

    firstcommit

---
+++ exercise-console_rawat-manish.js
@@ -0,0 +1,28 @@
+
+'use strict'
+const yo = 'yo.'
+const temperature = 21.3
+const fruits = ['apple', 'pear', 'orange']
+const temperatureStr = 'The temperature is'
+const collectionStr = 'The collection of fruits is '
+const html = '<h1> No HTML interpreter</h1>'
+const age = 18
+const age2 = 'eighteen'
+const bool = true
+const bool2 = null
+const groupStr = 'This is a group'
+const groupContent = 'Group content'
+const secondGroup = 'This is a second group (closed by default)'
+const secondContent = 'Content of the second group'
+
+console.log(yo)
+console.log(temperature)
+console.log(fruits)
+console.log(temperatureStr)
+console.log(collectionStr)
+console.log(html)
+console.log(age, age2, bool, bool2, undefined)
+console.log(groupStr)
+console.log(groupContent)
+console.log(secondGroup)
+console.log(secondContent)
